d0d0143220f6be59922d78ab7e5412b68fcdfd65
Vad_Siam <nevod80@gmail.com>
2023-04-23 15:29:23 +0700

    added fix

--- src/components/UserDashboard/NewDropsAndActions/DropActionCard.tsx
+++ src/components/UserDashboard/NewDropsAndActions/DropActionCard.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Card, CardContent, Typography, CardMedia } from '@mui/material';
+import { Card, CardContent, Typography, CardMedia, Avatar } from '@mui/material';
 import { DropAction } from '../../../types/UserDashboardTypes';
+import { selectTokens } from '../../../app/slices/selectors';
+import { useSelector } from 'react-redux';
 
 
 interface NewsCardProps {
@@ -9,24 +11,41 @@ interface NewsCardProps {
 }
 
 const DropActionCard: React.FC<NewsCardProps> = ({ dropAction, onClick }) => {
+  const { title, imageUrl, id } = dropAction;
+  const whichToken = id.includes('adds')
+    ? 'adds'
+    : id.includes('prda')
+      ? 'prda' : 'gucc';
+  const tokens = useSelector(selectTokens);
+  const tokenImg = tokens.find((token) => token.symbol.toLowerCase() === whichToken)?.imageUrl;
+
   return (
     <Card onClick={onClick} >
       <CardMedia
         component="img"
         height="140"
-        image={dropAction.imageUrl}
-        alt={dropAction.title}
+        image={imageUrl}
+        alt={title}
       />
       <CardContent
         sx={{
-          minHeight: '200px',
+          minHeight: '260px',
           display: 'flex',
           flexDirection: 'column',
           justifyContent: 'space-between',
         }}
       >
-        <Typography variant="h5" component="div">
-          {dropAction.title}
+        <Typography
+          variant="h5"
+          component="div"
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar src={tokenImg} sx={{ width: 40, height: 40, marginRight: 1 }} />
+          {title}
         </Typography>
         <div>
           read more...
